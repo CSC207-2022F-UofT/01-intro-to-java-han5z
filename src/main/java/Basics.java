f69ec1ddf4e31b8899e10d80dc979683227a5ae9
han5z <lexmic2015@gmail.com>
2022-10-09 19:14:43 -0400

    First commit

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -40,10 +40,7 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -58,11 +55,7 @@ public class Basics {
          *    (Relevant reading: 1.2. Variables and Types)
          */
 
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +92,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +139,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] split_strings = to_split.split(" ", 7);
+        for (String a : split_strings){
+            ret.append(a.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -164,13 +164,9 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
-
+        for (int i = 1; i < arr.length; i += 2) {
+            current_sum += arr[i];
+        }
         return current_sum;
     }
 
